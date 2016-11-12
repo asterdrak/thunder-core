7175c4d0fb8679001ca294adca71e5593065c96e
asterdrak <asterdrak@gmail.com>
2016-11-12 10:52:02 +0100

    changes main program dir to lib adds Environment class and some test code

---
+++ lib/ThunderCore/App.php
@@ -0,0 +1,23 @@
+<?php
+namespace ThunderCore;
+
+/**
+* 
+*/
+class App// extends AnotherClass
+{
+    private $ENVIRONMENT;
+
+    function __construct(argument) {
+      $this->ENVIRONMENT = $ENVIRONMENT;
+      echo $ENVIRONMENT;
+      new Environment;
+    }
+
+    public function set_environment_variables() {
+    }
+
+    public function start() {
+        return 'Hallo!';
+    }
+}
---
+++ lib/ThunderCore/Environment.php
@@ -0,0 +1,16 @@
+<?php
+namespace ThunderCore;
+
+/**
+* 
+*/
+class Environment// extends AnotherClass
+{
+  
+  function __construct()
+  {
+    echo "konstruktor ClassNAme";
+  }
+}
+
+?>
