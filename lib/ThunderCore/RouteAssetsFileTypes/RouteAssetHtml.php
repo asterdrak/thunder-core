1df088c69171ff1abb318f5f61240cb617a93ab0
asterdrak <asterdrak@gmail.com>
2017-03-19 17:59:49 +0100

    sets up rooting strategy for html files

---
+++ lib/ThunderCore/RouteAssetsFileTypes/RouteAssetHtml.php
@@ -0,0 +1,22 @@
+<?php
+namespace ThunderCore\RouteAssetsFileTypes;
+
+use \ThunderCore\Abstracts\RouteAssetStrategy;
+
+/**
+* sets up rooting strategy for html files
+*/
+class RouteAssetHtml implements RouteAssetStrategy
+{
+
+  function return_file(string $filename, string $extension)
+  {
+      header('Content-Type: text/html');
+      header('Expires: 0');
+      header('Cache-Control: must-revalidate');
+      header('Pragma: public');
+      header('Content-Length: ' . filesize($filename));
+      readfile($filename);
+      exit;
+  }
+}
